3d8b139913d8f2a5a70704c633242d96c9a3fa3f
matihernandez90 <55144153+matihernandez90@users.noreply.github.com>
2024-07-03 14:43:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelector('.carousel');
-    M.Carousel.init(elementosCarousel,{
+    const carouselInstance = M.Carousel.init(elementosCarousel, {
         duration: 300,
-        dist:-30,
+        dist: -30,
         shift: 5,
         padding: -5,
         numVisible: 5,
         indicators: true,
         noWrap: false,
-
     });
+
+    setInterval(() => {
+        const instance = M.Carousel.getInstance(elementosCarousel);
+        instance.next();
+    }, 3000); // Cambia la imagen cada 3 segundos
 
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 
+    var sidenavElems = document.querySelectorAll('.sidenav');
+    var sidenavInstances = M.Sidenav.init(sidenavElems);
 });
-
